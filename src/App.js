e72bd7df807e024c0a8395eebf44933891997867
Hasan Dawood <hasandawood555@gmail.com>
2020-06-19 02:26:30 +0500

    Made changings in App.js, App.css, studentdetails.js and index.html

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import StudentDetails from './studentdetails';
 
 
 function App() {
 
-   //let [count, setCount] = useState(1);
-
-  var SName = prompt("What is your name?");
-  var FName = prompt("What is your father name?");
-  var Course = prompt("Which course you are taking?");
-  var Age = prompt("What is your age?");
-
+  let [count, setCount] = useState(0);
+  let [isDark, setDark] = useState(true);
 
   return (
-    <div className="App">
+    <div className={`lightTheme ${isDark ? 'darkTheme' : ''}`}>
 
-      <StudentDetails name={SName} Fname={FName} course={Course} age={Age} />
+      <h1>Theme : {isDark ? 'Dark' : 'Light'}</h1>
+      <button onClick={() => setDark(!isDark)}>change theme</button>
 
-      {/* <h1>Value of counter variable is : {count}</h1>
-      <button onClick={
-        () => setCount(++count)
-      }>
-        Increase
+      <hr />
+
+      <h1>Value of counter variable is : {count}</h1>
+      <div>
+        <button onClick={
+          () => setCount(++count)
+        }>
+          Increase
           </button>
 
-      <button onClick={
-        () => setCount(--count)
-      }>
-        increase
+        <button onClick={
+          () => setCount(--count)
+        }>
+          Decrease
           </button>
 
+        <button onClick={
+          () => setCount(count - count)
+        }>
+          Reset
+          </button>
+      </div>
 
-      <button onClick={
-        () => setCount(count - count)
-      }>
-        Reset
-          </button>  */}
+      <hr />
 
-      
+      <StudentDetails name="Hasan Dawood" Fname="Hasan Adil" course="BootCamp2020" age="21" />
 
     </div>
   );
--- src/studentdetails.js
+++ src/studentdetails.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function StudentDetails(props) {
-
     return (
         <div>
+            <h1>Student Details</h1>
             <p><b>Name : </b> {props.name}</p>
             <p><b>Father Name : </b> {props.Fname}</p>
             <p><b>Course : </b> {props.course}</p>
